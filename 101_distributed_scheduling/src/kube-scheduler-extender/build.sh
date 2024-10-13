b5feeb2e06cfb7a905d59d5b6d680992c7ab16d3
kafkaqin <kafkaqin@gmail.com>
2024-10-13 09:26:00 +0800

    add code

--- 101_distributed_scheduling/src/kube-scheduler-extender/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# 构建 Docker 镜像
-docker build -t scheduler-extender:1.0 .
-
--- 101_distributed_scheduling/src/kube-scheduler-extender/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# 启动Docker容器
-docker run --rm -v /path/to/kubeconfig:/path/to/kubeconfig:ro my-golang-app
